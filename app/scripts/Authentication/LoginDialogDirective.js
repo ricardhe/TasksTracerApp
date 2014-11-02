3d474fdd38dd10c3414195dba7b239f01203ba18
ricardhe <ricardh@gmail.com>
2014-11-03 00:11:41 +0100

    diumenge

--- app/scripts/Authentication/LoginDialogDirective.js
+++ app/scripts/Authentication/LoginDialogDirective.js
@@ -1,17 +1,54 @@
 ﻿
 
-app.directive('loginDialog', function (AUTH_EVENTS) {
+app.directive('logindialog', function (AUTH_EVENTS) {
     return {
-        restrict: 'A',
-        template: '<div ng-if="visible" ng-include="\'login-form.html\'">',
-    link: function (scope) {
-        var showDialog = function () {
+        restrict: 'EA',
+        templateUrl: 'scripts/Authentication/login-form.html',
+        scope: {
+            message:'@'
+        },
+        link: function (scope) {
+            /*
+            var showDialog = function () {
+                scope.visible = true;
+            };
+
             scope.visible = true;
-        };
-  
-        scope.visible = false;
-        scope.$on(AUTH_EVENTS.notAuthenticated, showDialog);
-        scope.$on(AUTH_EVENTS.sessionTimeout, showDialog);
+            scope.$on(AUTH_EVENTS.notAuthenticated, showDialog);
+            scope.$on(AUTH_EVENTS.sessionTimeout, showDialog);
+            */
+        },
+        controller: ['$scope', 'AuthService', function ($scope,AuthService) {
+            'use strict';
+            var username = "ricardh@gmail.com";
+            var password = "Valencia2!";
+            var confirmpassword = "Valencia2!";
+
+            $scope.user = { username: username, password: password };
+            //$scope.message = '';
+            message = '';
+
+
+            $scope.submit = function () {
+
+                var loginData = {
+                    grant_type: 'password',
+                    username: $scope.user.username,
+                    password: $scope.user.password
+                };
+
+                AuthService.Login(loginData).then(
+                    function (data) {
+                        message = 'Welcome ' + username;
+                    },
+                    function (error) {
+                        message = 'An error ocurred while trying to get loged';
+                    }
+                    );
+
+            };
+        }]
     }
-};
-})
+});
+
+//'<div ng-if="visible" ng-include="\'login-form.html\'">',
